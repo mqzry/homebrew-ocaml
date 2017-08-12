e125497c278951ff11a8ebc491d9ab6b1e0d721a
Mohamed Hashi <mhas@spreadshirt.net>
2017-08-12 13:44:50 +0200

    added sha for 4.04

--- Formula/ocaml@4.04.rb
+++ Formula/ocaml@4.04.rb
@@ -3,4 +3,5 @@ require File.expand_path("../../Abstract/abstract-ocaml", __FILE__)
 class OcamlAT404 < AbstractOCaml
   init
   url "https://github.com/ocaml/ocaml/archive/4.04.2.tar.gz"
+  sha256 "6277a477956fc7b76f28af9941dce2984d0df809a0361093eb2e28234bf9c8ed"
 end
