3545d2f4fe184a8a1d64fae228ef22c8118f2bd5
Mohamed Hashi <mhas@spreadshirt.net>
2017-08-12 13:09:59 +0200

    first commit. Added abstract class and first stab at an implementation.

---
+++ Abstract/abstract_ocaml.rb
@@ -0,0 +1,49 @@
+# Mostly from homebrew-core/Formula/ocaml.rb
+require "formula"
+
+class Ocaml < Formula
+  def self.init
+    desc "General purpose programming language in the ML family"
+    homepage "https://ocaml.org/"
+    head "https://caml.inria.fr/svn/ocaml/trunk", :using => :svn
+
+    pour_bottle? do
+      # The ocaml compilers embed prefix information in weird ways that the default
+      # brew detection doesn't find, and so needs to be explicitly blacklisted.
+      reason "The bottle needs to be installed into /usr/local."
+      satisfy { HOMEBREW_PREFIX.to_s == "/usr/local" }
+    end
+
+    bottle do
+      cellar :any_skip_relocation
+      sha256 "0aa92cba5d37a2dd0625b0210a09f12218443c9c806ee04a9988a1041a54b5bc" => :sierra
+      sha256 "58675349ab224e93c8f9470e98277526b2aafd3721f684ac451a3a1e187ec9f7" => :el_capitan
+      sha256 "a8b02428804a20627265ba737aca7800eb565907c1d07bc8bbcf68afedb97cb1" => :yosemite
+    end
+
+    option "with-x11", "Install with the Graphics module"
+    option "with-flambda", "Install with flambda support"
+
+    depends_on :x11 => :optional
+  end
+  
+  def install
+    ENV.deparallelize # Builds are not parallel-safe, esp. with many cores
+
+    # the ./configure in this package is NOT a GNU autoconf script!
+    args = ["-prefix", HOMEBREW_PREFIX.to_s, "-with-debug-runtime", "-mandir", man]
+    args << "-no-graph" if build.without? "x11"
+    args << "-flambda" if build.with? "flambda"
+    system "./configure", *args
+
+    system "make", "world.opt"
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    output = shell_output("echo 'let x = 1 ;;' | #{bin}/ocaml 2>&1")
+    assert_match "val x : int = 1", output
+    assert_match HOMEBREW_PREFIX.to_s, shell_output("#{bin}/ocamlc -where")
+  end
+
+end
--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# homebrew-ocaml
-Tab for different ocaml versions
+# Homebrew PHP
+
+A centralized repository for all OCaml versions.
+
+## Requirements
+
+* [Homebrew](https://github.com/Homebrew/brew)
+
+## Installation
+
+Run the following in your command-line:
+
+```sh
+$ brew tap mqzry/homebrew-ocamml
+```
+
+## Background
+
+This repository contains formulae for  different versions of OCaml formulae for [Homebrew](https://github.com/Homebrew/brew).
+
+## Usage
+
+**Note:** For a list of available configuration options run:
+
+```sh
+$ brew options php56
+```
+
+Once the tap is installed, you can install `php53`, `php54`, `php55`, `php56`, `php70`, `php71`, `php72`, or any formulae you might need via:
+
+```sh
+$ brew install php56
+```
+
+That's it!
+
+Please also follow the instructions from `brew info` at the end of the install to ensure you properly installed your PHP version.
+
+## Todo
+
+
+
+## License
+
+See [LICENSE](LICENSE).
