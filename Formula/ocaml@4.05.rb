ced5e8c73ed1bd52a8bd916e607479bb40d9761a
Mohamed Hashi <mhas@spreadshirt.net>
2017-08-12 13:25:16 +0200

    Follow naming convention of homebrew

--- Formula/ocaml@4.05.rb
+++ Formula/ocaml@4.05.rb
@@ -1,6 +1,6 @@
 require File.expand_path("../../Abstract/abstract-ocaml", __FILE__)
 
-class OCamlAT405 < AbstractOCaml
+class OcamlAT405 < AbstractOCaml
   init
   url "https://caml.inria.fr/pub/distrib/ocaml-4.05/ocaml-4.05.0.tar.xz"
   sha256 "04a527ba14b4d7d1b2ea7b2ae21aefecfa8d304399db94f35a96df1459e02ef9"
